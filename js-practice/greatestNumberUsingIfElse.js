94128e8aaf2c761e15d4283a6e7b903cf0cca31d
Haider-GitHubRepo <161403424+Haider-GitHubRepo@users.noreply.github.com>
2025-09-19 14:27:07 +0530

    Create greatestNumberUsingIfElse.js

---
+++ js-practice/greatestNumberUsingIfElse.js
@@ -0,0 +1,14 @@
+let a = 297, b = 31, c = 25;
+if ( a > b ) {
+    if ( a > c ){
+        console.log( a )
+    }else{
+        console.log( c )
+    }
+}else{
+    if ( b > c ){
+        console.log( b )
+    }else{
+        console.log( c )
+    }
+}
