b6b8782625068b713a89d7700e9b93fbf8cf69f2
Haider-GitHubRepo <161403424+Haider-GitHubRepo@users.noreply.github.com>
2025-09-19 14:19:41 +0530

    Create ifElse.js

---
+++ js-practice/ifElse.js
@@ -0,0 +1,7 @@
+let kartik;
+kartik = " not hungry"
+if (kartik === "hungry") {
+    console.log("Kartik is ",kartik,". He needs break.");
+} else {
+    console.log("Kartik is ",kartik,". He doesn't need break.");
+}
