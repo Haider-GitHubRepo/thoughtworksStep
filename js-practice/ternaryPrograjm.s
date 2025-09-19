6459ea7fe9bb941cb4f04b9aae4e4dc581145580
Haider-GitHubRepo <161403424+Haider-GitHubRepo@users.noreply.github.com>
2025-09-19 14:21:52 +0530

    Create ternaryPrograjm.s

---
+++ js-practice/ternaryPrograjm.s
@@ -0,0 +1,3 @@
+let salary = 15000;
+let shouldIBuyIPhone = ( salary < 150000 ) ? "sell your kidney and buy an I-Phone"  : "Yeah you should buy if you don't have any";
+console.log(shouldIBuyIPhone);
