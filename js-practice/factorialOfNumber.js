a0e5d56e965795e357175ec668b7eeb58aecfac2
Haider-GitHubRepo <161403424+Haider-GitHubRepo@users.noreply.github.com>
2025-09-19 14:36:29 +0530

    Create factorialOfNumber.js

---
+++ js-practice/factorialOfNumber.js
@@ -0,0 +1,8 @@
+let fact = 1;
+let currentNumber = 1;
+let finalNumber = 6;
+while(currentNumber <= finalNumber ){
+    fact = fact * currentNumber;
+    currentNumber++;
+}                   
+console.log("Factorial of ",finalNumber," = ",fact);
