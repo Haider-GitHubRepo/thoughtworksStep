17e0e52dfd7391819cb676a83819013c53ce0e01
Haider-GitHubRepo <161403424+Haider-GitHubRepo@users.noreply.github.com>
2025-09-19 14:39:54 +0530

    Create secondGreatestNumber.js

---
+++ js-practice/secondGreatestNumber.js
@@ -0,0 +1,4 @@
+let a=1111,b=892,c=33128;
+let secondGreatestNumber = ((a>b)? a : b) > ((b>c)? b : c) ? ((b>c)? b : c) :  (a>b)? a : b           
+console.log("Second Greatest Number is ",secondGreatestNumber);
+                 
