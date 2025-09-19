c4a850307c01d648eb36bd076f650f78fd6521f9
Haider-GitHubRepo <161403424+Haider-GitHubRepo@users.noreply.github.com>
2025-09-19 14:34:25 +0530

    Create switchProgram.js

---
+++ js-practice/switchProgram.js
@@ -0,0 +1,14 @@
+let menuOption = 2;
+switch(menuOption){
+    case 1:
+        console.log("Aalu Matar");
+        break;
+    case 2:
+        console.log("Aalu Paratha");
+        break;
+    case 3:
+        console.log("Mutton Biryani");
+        break;
+    default:
+        console.log("Kuch Khane ko nahi milega");
+}
