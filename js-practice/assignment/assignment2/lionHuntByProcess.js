22c3322f4c155a6a6aa0252f4d06c3c4119fdd80
Haider-GitHubRepo <161403424+Haider-GitHubRepo@users.noreply.github.com>
2025-09-21 13:21:02 +0530

    code added to check if there are only lions or zebras

--- js-practice/assignment/assignment2/lionHuntByProcess.js
+++ js-practice/assignment/assignment2/lionHuntByProcess.js
@@ -1,21 +1,40 @@
+
 const testCase1 = "";
 const testCase2 = "      ";
+const testCase3 = " LL L L";
+const testCase4 = " ZZ Z Z";
 
-const testCaseToUse = testCase2;
+const testCaseToUse = testCase4;
 const testCaseLength = testCaseToUse.length;
+const lion = "L";
+const zebra = "Z";
+
 let minimumDistance;
 
 let isTestCaseEmpty = true;
 let containsOnlySpaces = true;
+let onlyLionInSavannah = true;
+let onlyZebraInSavannah = true;
 
-for (let currentCharacter = 0; currentCharacter < testCaseLength; currentCharacter++) {
+for (let currentAnimal = 0; currentAnimal < testCaseLength; currentAnimal++) {
+
     isTestCaseEmpty = false;
-    if (testCaseToUse[currentCharacter] !== " ") {
+
+    if (testCaseToUse[currentAnimal] === zebra) {
+        onlyLionInSavannah = false;
+    }
+    if (testCaseToUse[currentAnimal] === lion) {
+        onlyZebraInSavannah = false;
+    }
+    if (testCaseToUse[currentAnimal] !== " ") {
         containsOnlySpaces = false;
     }
 }
 
-if (isTestCaseEmpty || containsOnlySpaces) {
+
+
+if (isTestCaseEmpty || containsOnlySpaces || onlyLionInSavannah || onlyZebraInSavannah) {
     minimumDistance = -1;
 }
 console.log("Input: ", testCaseToUse, "Output: ", minimumDistance);
+
