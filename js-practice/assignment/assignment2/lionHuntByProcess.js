00f00916ef8541228532b7f3921f366a73d240a9
Haider-GitHubRepo <161403424+Haider-GitHubRepo@users.noreply.github.com>
2025-09-21 13:23:32 +0530

    code added to find the minimum distance between a zebra and a lion

--- js-practice/assignment/assignment2/lionHuntByProcess.js
+++ js-practice/assignment/assignment2/lionHuntByProcess.js
@@ -1,37 +1,54 @@
 
+
 const testCase1 = "";
 const testCase2 = "      ";
 const testCase3 = " LL L L";
 const testCase4 = " ZZ Z Z";
+const testCase5 = "L  ZL Z";
+const testCase6 = "LZ";
+const testCase7 = "Z   L   Z";
+const testCase8 = "L     Z";
+const testCase9 = "Z L";
+const testCase10 = "   L  ZL Z";
+const testCase11 = "   ZL L  Z   Z   L LZ L Z  ZL ZL Z Z ZL LZ"
 
-const testCaseToUse = testCase4;
+const testCaseToUse = testCase1;
 const testCaseLength = testCaseToUse.length;
 const lion = "L";
 const zebra = "Z";
 
-let minimumDistance;
+let minimumDistance = testCaseLength;
 
 let isTestCaseEmpty = true;
 let containsOnlySpaces = true;
 let onlyLionInSavannah = true;
 let onlyZebraInSavannah = true;
 
+let lionFoundAt;
+let zebraFoundAt;
+
 for (let currentAnimal = 0; currentAnimal < testCaseLength; currentAnimal++) {
 
     isTestCaseEmpty = false;
 
-    if (testCaseToUse[currentAnimal] === zebra) {
-        onlyLionInSavannah = false;
-    }
     if (testCaseToUse[currentAnimal] === lion) {
+        lionFoundAt = currentAnimal;
         onlyZebraInSavannah = false;
-    }
-    if (testCaseToUse[currentAnimal] !== " ") {
         containsOnlySpaces = false;
     }
+
+    if (testCaseToUse[currentAnimal] === zebra) {
+        zebraFoundAt = currentAnimal;
+        onlyLionInSavannah = false;
+        containsOnlySpaces = false;
+    }
+
+    let currentDistance = (lionFoundAt > zebraFoundAt) ? lionFoundAt - zebraFoundAt - 1 : zebraFoundAt - lionFoundAt - 1;
+
+    if (currentDistance < minimumDistance) {
+        minimumDistance = currentDistance;
+    }
 }
-
-
 
 if (isTestCaseEmpty || containsOnlySpaces || onlyLionInSavannah || onlyZebraInSavannah) {
     minimumDistance = -1;
