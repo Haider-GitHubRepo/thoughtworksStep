ea6fd016596c0407fa5ae1c3b7cfd5caba368ed6
Haider-GitHubRepo <161403424+Haider-GitHubRepo@users.noreply.github.com>
2025-09-21 13:15:34 +0530

    code to check if test case is empty ""

---
+++ js-practice/assignment/assignment2/lionHuntByProcess.js
@@ -0,0 +1,12 @@
+const testCase = "";
+
+const testCaseToUse = testCase;
+
+const testCaseLength = testCase.length;
+let minimumDistance;
+
+if (testCaseLength === 0) {
+    minimumDistance = -1;
+}
+
+console.log("Input: ", testCaseToUse, "Output: ", minimumDistance);
