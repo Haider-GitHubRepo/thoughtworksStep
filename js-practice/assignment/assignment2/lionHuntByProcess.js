5f64b4083fa124ae04e0f6bdb637110c6d88d4f8
Haider-GitHubRepo <161403424+Haider-GitHubRepo@users.noreply.github.com>
2025-09-21 13:19:18 +0530

    code added to check if test case contains only spaces

--- js-practice/assignment/assignment2/lionHuntByProcess.js
+++ js-practice/assignment/assignment2/lionHuntByProcess.js
@@ -1,12 +1,21 @@
-const testCase = "";
+const testCase1 = "";
+const testCase2 = "      ";
 
-const testCaseToUse = testCase;
-
-const testCaseLength = testCase.length;
+const testCaseToUse = testCase2;
+const testCaseLength = testCaseToUse.length;
 let minimumDistance;
 
-if (testCaseLength === 0) {
-    minimumDistance = -1;
+let isTestCaseEmpty = true;
+let containsOnlySpaces = true;
+
+for (let currentCharacter = 0; currentCharacter < testCaseLength; currentCharacter++) {
+    isTestCaseEmpty = false;
+    if (testCaseToUse[currentCharacter] !== " ") {
+        containsOnlySpaces = false;
+    }
 }
 
+if (isTestCaseEmpty || containsOnlySpaces) {
+    minimumDistance = -1;
+}
 console.log("Input: ", testCaseToUse, "Output: ", minimumDistance);
